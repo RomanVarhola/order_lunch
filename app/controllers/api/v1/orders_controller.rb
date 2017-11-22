efa216891402b00f7ab3bd414def4834b589a214
Roman Varhola <roman.varhola@gmail.com>
2017-11-22 03:37:50 +0200

    add scope

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class OrdersController < ApplicationController
       def index
-        @orders = Order.where(date: DateTime.now.to_date)
+        @orders = Order.today_order
         render jsonapi: @orders, include: [:user, :food, :drink], status: :ok
       end
     end
